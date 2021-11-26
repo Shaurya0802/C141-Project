a1fb40ee40f4f0bdb4e5b5594fbcdaee6c84fde9
unknown <shauryaakhileshsharma@gmail.com>
2021-11-26 08:44:37 +0530

    C141-Project

---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request
+import csv
+
+all_articles = []
+
+with open("articles.csv", encoding="utf8") as f:
+    reader = csv.reader(f)
+    data = list(reader)
+
+    all_articles = data[1:]
+
+liked_articles = []
+unliked_articles = []
+
+app = Flask(__name__)
+
+@app.route("/get-article")
+def get_article():
+    return jsonify({
+        "data": all_articles[0],
+        "status": "Success"
+    })
+
+@app.route("/liked-article", methods=["POST"])
+def liked_article():
+    global all_articles
+    article = all_articles[0]
+    all_articles = all_articles[1:]
+    liked_articles.append(article)
+
+    return ({
+        "status": "Success"
+    }), 201
+
+@app.route("/unliked-article", methods=["POST"])
+def unliked_article():
+    global all_articles
+    article = all_articles[0]
+    all_articles = all_articles[1:]
+    unliked_articles.append(article)
+
+    return ({
+        "status": "Success"
+    }), 201
+
+if __name__ == "__main__":
+    app.run()
